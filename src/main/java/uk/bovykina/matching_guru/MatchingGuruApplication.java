8715dcb15fc58b9d2de26056e38847a7ce21c154
Polina Bovykina <paulina.northy@gmail.com>
2024-11-18 14:47:58 +0100

    init

---
+++ src/main/java/uk/bovykina/matching_guru/MatchingGuruApplication.java
@@ -0,0 +1,13 @@
+package uk.bovykina.matching_guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/bovykina/matching_guru/MatchingGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.bovykina.matching_guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
