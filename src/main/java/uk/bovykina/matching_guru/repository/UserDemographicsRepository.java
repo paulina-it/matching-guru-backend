6692e5fa8e181d2e2aadbe61f03c4795fd7e2eb2
Polina Bovykina <paulina.northy@gmail.com>
2025-02-16 17:44:25 +0000

    user_demographics merged with users entity and removed as a separate class

--- src/main/java/uk/bovykina/matching_guru/repository/UserDemographicsRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.bovykina.matching_guru.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uk.bovykina.matching_guru.entity.UserDemographics;
-
-public interface UserDemographicsRepository extends JpaRepository<UserDemographics, Long> {
-}
