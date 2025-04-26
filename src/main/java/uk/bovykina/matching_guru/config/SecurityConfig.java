85278b851e3189fe94c6efd2b599e2d228ffd54a
Polina <paulina.northy@gmail.com>
2025-04-26 22:27:13 +0100

    deploy prep: cors fixed

--- src/main/java/uk/bovykina/matching_guru/config/SecurityConfig.java
+++ src/main/java/uk/bovykina/matching_guru/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "https://matching-guru.vercel.app/", "https://matching-guru-backend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
         configuration.setAllowCredentials(true);
